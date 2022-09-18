31adbbbba465f2012c2afcd1ea87a724407939c6
HsuTing <hsuting0106@gmail.com>
2022-09-18 07:32:54 +0000

    feat(changelog-generator): could get the latest tag

--- changelog-generator/index.js
+++ changelog-generator/index.js
@@ -4,6 +4,15 @@ const github = require('@actions/github');
 const { repo } = github.context;
 const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
 
+const getFrom = async from => {
+  if (from !== 'latest tag')
+    return from;
+
+  const { data: [{ name }] } = await octokit.rest.repos.listTags(repo);
+
+  return name;
+};
+
 const getPullRequestNumbers = async basehead => {
   const { data: { commits } } = await octokit.request(
     'GET /repos/{owner}/{repo}/compare/{basehead}', {
@@ -39,8 +48,11 @@ const getPullRequests = pullRequestNumbers =>
 
 (async () => {
   try {
-    // TODO: should remove basehead
-    const pullRequestNumbers = await getPullRequestNumbers('v1.0.0...HEAD');
+    // TODO: should remove default value
+    const from = await getFrom(core.getInput('from') || 'latest tag');
+    const to = core.getInput('to') || 'HEAD';
+
+    const pullRequestNumbers = await getPullRequestNumbers(`${from}...${to}`);
     const pullRequests = await getPullRequests(pullRequestNumbers);
 
     console.log(pullRequests)
