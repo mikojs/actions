75356351ffd6cbbad2344306b411e8e7f3d0cbfd
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2022-09-19 01:02:22 +0800

    (changelog-generator) Could generate the changelog (#2)
    * ci(github): add pre-releae workflow
    
    * feat(changelog-generator): add changelog-generator action
    
    * feat(changelog-generator): could get all pull request numbers
    
    * feat(changelog-generator): could get pull requests
    
    * feat(changelog-generator): could get the latest tag
    
    * feat(changelog-generator): add error when no tag is found
    
    * feat(changelog-generator): could categorize the pull requets
    
    * perf(changelog-generator): could render release output and remove default value
    
    * feat(changelog-generator): could generator changelog detail
    
    * feat(changelog-generator): could generate log from an array
    
    * feat(changelog-generator): could generator the changelog

---
+++ changelog-generator/index.js
@@ -0,0 +1,196 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { format } = require('date-fns');
+
+const { repo } = github.context;
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+const releaseInfo = {};
+
+// TODO: support monorepo
+const initializeRelease = configStr => {
+  const config = JSON.parse(configStr);
+
+  Object.keys(config)
+    .forEach(key => {
+      releaseInfo[key] = {
+        title: config[key],
+        items: [],
+      };
+    });
+
+  if (!releaseInfo.uncategorized)
+    releaseInfo.uncategorized = {
+      title: ':question: Uncategorized',
+      items: [],
+    };
+};
+
+const getFrom = async from => {
+  if (from !== 'latest tag')
+    return from;
+
+  const { data: [{ name } = {}] } = await octokit.rest.repos.listTags(repo);
+
+  if (!name)
+    throw new Error('Here should have at least one tag');
+
+  return name;
+};
+
+const getPullRequestNumbers = async basehead => {
+  const { data: { commits } } = await octokit.request(
+    'GET /repos/{owner}/{repo}/compare/{basehead}', {
+      ...repo,
+      basehead,
+    },
+  );
+
+  return commits.reduce((result, { commit: { message }}) => {
+    const pullRequestNumber = message
+      .replace(/\*.+/g, '')
+      .match(/\(#\d+\)/)
+      ?.[0]
+      .replace(/\(#(.+)\)/, (_, p1) => p1);
+
+    return !pullRequestNumber
+      ? result
+      : [...result, pullRequestNumber];
+  }, []);
+};
+
+const loadPullRequests = pullRequestNumbers =>
+  pullRequestNumbers.reduce(
+    async (resultP, pullRequestNumber) => {
+      await resultP;
+      core.debug(`loading ${pullRequestNumber}`);
+
+      const { data: { html_url: url, title, body, labels, user } } = await octokit.rest.pulls.get({
+        ...repo,
+        pull_number: pullRequestNumber,
+      });
+      const item = {
+        number: pullRequestNumber,
+        url,
+        title,
+        // TODO: body could overwrite title
+        body,
+        user,
+      };
+
+      labels.forEach(({ name }) => {
+        const { items } = (releaseInfo[name] || releaseInfo.uncategorized);
+
+        if (items.some(({ number }) => number === item.number))
+          return;
+
+        items.unshift(item);
+      });
+      core.debug(`${pullRequestNumber} is added`);
+    },
+    Promise.resolve(),
+  );
+
+const getTagDate = async tag => {
+  const {
+    repository: {
+      ref: { target },
+    },
+  } = await octokit.graphql(`
+    query getTagDate($owner: String!, $repo: String!, $qualifiedName: String!) {
+      repository(owner: $owner, name: $repo) {
+        ref(qualifiedName: $qualifiedName) {
+          id
+          target {
+            typename: __typename
+            ... on Tag {
+              id
+              tagger {
+                date
+              }
+            }
+            ... on Commit {
+              id
+              committer {
+                date
+              }
+            }
+          }
+        }
+      }
+    }
+  `, {
+    ...repo,
+    qualifiedName: `refs/tags/${tag}`,
+  });
+
+  return new Date(
+    target.typename === 'Tag'
+      ? target.tagger.date
+      : target.committer.date,
+  );
+};
+
+const renderRelease = async tag => {
+  const nextVersion = tag === 'HEAD'
+    ? core.getInput('next version')
+    : tag;
+  const date = tag === 'HEAD'
+    ? new Date()
+    : await getTagDate(tag);
+  const release = [
+    `## ${nextVersion} (${format(date, 'yyyy-MM-dd')})`,
+    ...Object.keys(releaseInfo)
+      .filter(key => releaseInfo[key].items.length !== 0)
+      .map(key => [
+        '',
+        `#### ${releaseInfo[key].title}`,
+        ...releaseInfo[key].items.map(
+          ({ number, url, title, user }) => [
+            '-',
+            `[#${number}](${url})`,
+            title.replace(/[ ]+$/, ''),
+            `([@${user.login}](${user.html_url}))`,
+          ].join(' '),
+        ),
+      ].join('\n')),
+  ].join('\n');
+
+  Object.keys(releaseInfo)
+    .forEach(key => {
+      releaseInfo[key].items = [];
+    });
+  core.info(release);
+
+  return release;
+};
+
+(async () => {
+  try {
+    initializeRelease(core.getInput('config'));
+
+    const releases = await core.getMultilineInput('tags')
+      .reduce(async (resultP, tag, index, tags) => {
+        const result = await resultP;
+
+        if (index === 0)
+          return result;
+
+        const from = await getFrom(tags[index - 1]);
+
+        core.debug(`generating a release from ${from} to ${tag}`);
+        await loadPullRequests(
+          await getPullRequestNumbers(`${from}...${tag}`),
+        );
+
+        return [
+          ...result,
+          '',
+          await renderRelease(tag),
+        ];
+      }, Promise.resolve([]));
+
+    core.setOutput('relesae', releases.reverse().join('\n'));
+  } catch (e) {
+    core.setFailed(e.message);
+  }
+})();
