13a9af2b9bfc7ff157af740980d043843d98e28c
HsuTing <hsuting0106@gmail.com>
2022-09-19 03:39:34 +0000

    feat(chagnelog-generator): add loadInfo function

--- changelog-generator/index.js
+++ changelog-generator/index.js
@@ -1,64 +1,9 @@
 const core = require('@actions/core');
 const { format } = require('date-fns');
 
-const { repo, octokit } = require('./utils');
 const { getFrom, getPullRequestNumbers, getTagDate } = require('./getInfo');
+const loadInfo = require('./loadInfo');
 const releaseInfo = require('./releaseInfo');
-
-const loadPullRequests = pullRequestNumbers =>
-  pullRequestNumbers.reduce(
-    async (resultP, pullRequestNumber) => {
-      await resultP;
-      core.debug(`loading ${pullRequestNumber}`);
-
-      const {
-        data: { html_url: url, title, body, labels, user },
-      } = await octokit.rest.pulls.get({
-        ...repo,
-        pull_number: pullRequestNumber,
-      });
-      const {
-        data,
-      } = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
-        ...repo,
-        pull_number: pullRequestNumber,
-      });
-      const item = {
-        number: pullRequestNumber,
-        url,
-        title,
-        // TODO: body could overwrite title
-        body,
-        user,
-        workspaces: data.reduce(
-          (result, { filename }) => {
-            const key = Object.keys(workspacesInfo)
-              .find(worksapcesPath => filename.includes(worksapcesPath));
-
-            return !key || result.includes(workspacesInfo[key])
-              ? result
-              : [...result, workspacesInfo[key]];
-          },
-          [],
-        ),
-      };
-
-      if (item.workspaces.length === 0)
-        item.workspaces.push('notFound');
-
-      core.debug(item);
-      labels.forEach(({ name }) => {
-        const { items } = (releaseInfo[name] || releaseInfo.uncategorized);
-
-        if (items.some(({ number }) => number === item.number))
-          return;
-
-        items.unshift(item);
-      });
-      core.debug(`${pullRequestNumber} is added`);
-    },
-    Promise.resolve(),
-  );
 
 const renderRelease = async tag => {
   const nextVersion = tag === 'HEAD'
@@ -103,7 +48,7 @@ const renderRelease = async tag => {
         const from = await getFrom(tags[index - 1]);
 
         core.debug(`generating a release from ${from} to ${tag}`);
-        await loadPullRequests(
+        await loadInfo(
           await getPullRequestNumbers(`${from}...${tag}`),
         );
 
