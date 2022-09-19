e51c5b1b20a9a636525b41a42fa268aecdb4932c
HsuTing <hsuting0106@gmail.com>
2022-09-19 03:57:41 +0000

    feat(changelog-generator): add renderLabel

--- changelog-generator/renderRelease.js
+++ changelog-generator/renderRelease.js
@@ -4,6 +4,22 @@ const { format } = require('date-fns');
 const { getTagDate } = require('./getInfo');
 const releaseInfo = require('./releaseInfo');
 
+const renderLabel = ({ title, items }) =>
+  items.length === 0
+    ? []
+    : [
+        '',
+        `#### ${title}`,
+        items.map(
+          ({ number, url, title, user }) => [
+            '-',
+            `[#${number}](${url})`,
+            title.replace(/[ ]+$/, ''),
+            `([@${user.login}](${user.html_url}))`,
+          ].join(' '),
+        ),
+      ].flat();
+
 module.exports = async tag => {
   const nextVersion = tag === 'HEAD'
     ? core.getInput('next version')
@@ -13,21 +29,11 @@ module.exports = async tag => {
     : await getTagDate(tag);
   const release = [
     `## ${nextVersion} (${format(date, 'yyyy-MM-dd')})`,
-    ...Object.keys(releaseInfo.labels)
-      .filter(key => releaseInfo.labels[key].items.length !== 0)
-      .map(key => [
-        '',
-        `#### ${releaseInfo.labels[key].title}`,
-        ...releaseInfo.labels[key].items.map(
-          ({ number, url, title, user }) => [
-            '-',
-            `[#${number}](${url})`,
-            title.replace(/[ ]+$/, ''),
-            `([@${user.login}](${user.html_url}))`,
-          ].join(' '),
-        ),
-      ].join('\n')),
-  ].join('\n');
+    Object.values(releaseInfo.labels)
+      .map(renderLabel)
+      .flat(),
+  ].flat()
+    .join('\n');
 
   releaseInfo.reset();
   core.info(release);
