72cc9fe44069108181ad398491c8650170157383
HsuTing <hsuting0106@gmail.com>
2023-01-22 14:07:23 +0000

    feat(@mikojs/create-comment): would check if the bot comment exist before creating the comment

--- create-comment/index.js
+++ create-comment/index.js
@@ -1,21 +1,42 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-const token = core.getInput("token", { required: true });
-const comment = core.getInput("comment", { required: true });
-const issue = core.getInput("issue", { required: true });
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-const octokit = new github.GitHub(token);
+const token = core.getInput('token', { required: true });
+const comment = core.getInput('comment', { required: true });
+const issue = core.getInput('issue', { required: true });
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+const octokit = github.getOctokit(token);
 
-octokit.issues.createComment({
-  owner,
-  repo,
-  issue_number: issue,
-  body: comment,
-}).then(
-  ({ status }) => {
-    if (status < 200 || status >= 300)
-      core.setFailed(`Received status ${status} from API.`)
-  },
-  e => core.setFailed(e.message),
-);
+(async () => {
+  const comments = await octokit.rest.issues.listComments({
+    owner,
+    repo,
+    issue_number: issue,
+  });
+
+  if (!comments.data) {
+    core.setFailed(`Couldn't get the comments list.`);
+    return;
+  }
+
+  const botCommentExist = comments.data.some(
+    ({ user }) => user.login === 'github-actions[bot]',
+  );
+
+  if (botCommentExist) return;
+
+  octokit.issues
+    .createComment({
+      owner,
+      repo,
+      issue_number: issue,
+      body: comment,
+    })
+    .then(
+      ({ status }) => {
+        if (status < 200 || status >= 300)
+          core.setFailed(`Received status ${status} from API.`);
+      },
+      e => core.setFailed(e.message),
+    );
+})();
