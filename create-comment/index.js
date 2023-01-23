dd32a5c71e326a8b98866cd5269f9734830e5988
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2023-01-23 13:59:08 +0800

    Add PR check (#14)
    * style(github): add job name
    
    * style(github): change job name
    
    * style(github): modify job name
    
    * feat(github): add PR check
    
    * ci(.github): add PR check testing
    
    * fix(github): fix workflow error
    
    * ci(github): add comment to pr
    
    * fix(github): fix action error
    
    * fix(github): fix comment message
    
    * style(github): rename workflow
    
    * refactor(github): doesn't add comment
    
    * style(github): fix filename changed
    
    * style(github): change workflow name
    
    * feat(@mikojs/create-comment): add new action
    
    * feat(@mikojs/create-comment): copy babel add-comment action
    
    * feat(@mikojs/create-comment): would check if the bot comment exist before creating the comment
    
    * feat(github): use new create-comment action
    
    * feat(@mikojs/create-comment): add action
    
    * fix(github): fix error and add error message
    
    * fix(@mikojs/create-comment): fix filename
    
    * fix(@mikojs/create-comment): try to use npm
    
    * Revert "fix(@mikojs/create-comment): try to use npm"
    
    This reverts commit f0dab51d146718cf9e14b60c81ccd52f119ee565.
    
    * fix(@mikojs/create-comment): try to install the packages
    
    * fix(@mikojs/create-comment): fix createComment error
    
    * feat(@mikojs/create-comment): add pre script
    
    * feat(@mikojs/create-comment): could remove the comment
    
    * feat(@mikojs/create-comment): support to remove comment
    
    * Revert "feat(@mikojs/create-comment): add pre script"
    
    This reverts commit 9d827ce3bade87eeab9d25c67c63804797063bc6.
    
    * fix(github): fix argument error

---
+++ create-comment/index.js
@@ -0,0 +1,53 @@
+const childProcess = require("child_process");
+
+childProcess.execSync(`cd ${__dirname}; yarn`);
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const token = core.getInput('token', { required: true });
+const comment = core.getInput('comment', { required: true });
+const issue = core.getInput('issue', { required: true });
+const success = core.getBooleanInput('success', { required: true });
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+const octokit = github.getOctokit(token);
+
+(async () => {
+  const comments = await octokit.rest.issues.listComments({
+    owner,
+    repo,
+    issue_number: issue,
+  });
+
+  if (!comments.data) {
+    core.setFailed(`Couldn't get the comments list.`);
+    return;
+  }
+
+  const botComment = comments.data.find(
+    ({ user, body }) => user.login === 'github-actions[bot]' && body === comment,
+  );
+
+  if (botComment) {
+    if (success)
+      await octokit.rest.issues
+        .deleteComment({
+          owner,
+          repo,
+          comment_id: botComment.id,
+        });
+
+    return;
+  }
+
+  if (!success) {
+    await octokit.rest.issues
+      .createComment({
+        owner,
+        repo,
+        issue_number: issue,
+        body: comment,
+      });
+    core.setFailed(comment);
+  }
+})();
